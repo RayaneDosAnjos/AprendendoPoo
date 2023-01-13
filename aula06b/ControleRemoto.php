a8217ead83df542eae47fca8c8dee095e619a59b
RayaneDosAnjos <raydosanjos98@gmail.com>
2023-01-13 14:28:22 -0300

    Encapsulamento na prática com o projeto ControleRemoto

---
+++ aula06b/ControleRemoto.php
@@ -0,0 +1,93 @@
+<?php
+require_once 'controlador.php';
+class ControleRemoto implements Controlador {
+    //atributos
+    private $volume;
+    private $ligado;
+    private $tocando;
+
+    //Métodos especiais
+    public function __construct()
+    {
+        $this->volume=50;
+        $this->ligado=false;
+        $this->tocando=false;
+    }
+
+    private function getVolume(){
+        return  $this->volume;
+    }
+    private function setVolume($volume){
+        $this->volume=$volume;
+    }
+
+    private function getLigado(){
+        return $this->ligado;
+    }
+    private function setLigado($ligado){
+        $this->ligado=$ligado;
+    }
+
+    private function getTocando(){
+        return $this->tocando;
+    }
+    private function setTocando($tocando){
+        $this->tocando=$tocando;
+    }
+
+
+    public function ligar(){
+        $this->setLigado(true);
+    }
+    public function desligar(){
+        $this->setLigado(false);
+    }
+    public function abrirMenu(){
+        echo "<p>-----MENU-----</p>";
+        echo "<br>Está ligado?".($this->getLigado()?"Sim":"Não");
+        echo "<br>Está tocando?".($this->getTocando()?"Sim":"Não");
+        echo "<br>Volume: ".$this->getVolume();
+        for($i=0; $i <= $this->getVolume();$i+=10){
+            echo "|";
+        }
+        echo "<br>";
+    }
+    public function fecharMenu(){
+        echo "<br>Fechando Menu...";
+    }
+    public function maisVolume(){
+        if ($this->getLigado()) {
+            $this->setVolume($this->getVolume()+10);
+        }else{
+            echo "<p>ERRO! NÃO POSSO AUMENTAR O VOLUME.</p>";
+        }
+    }
+    public function menosVolume(){
+        if ($this->getLigado()) {
+            $this->setVolume($this->getVolume()-5);
+        }else{
+            echo "<p>ERRO! NÃO POSSO DIMINUIR O VOLUME</p>";
+        } 
+        
+    }
+    public function ligarMudo(){
+        if ($this->getLigado() && $this->getVolume()>0){
+            $this->setVolume(0);
+        }
+    }
+    public function desligarMudo(){
+        if ($this->getLigado() && $this->getVolume()==0) {
+            $this->setVolume(50);
+        }
+    }
+    public function play(){
+        if ($this->getLigado() && ! $this->getTocando()){
+            $this->getTocando(true);
+        }
+    }
+    public function pause(){
+        if ($this->getLigado() && $this->getTocando()){
+            $this->setTocando(false);
+        }
+    }
+}
